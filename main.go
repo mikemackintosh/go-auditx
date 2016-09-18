b8ddacaf097d3a6f60b028db3b4ed097539c087a
Mike Mackintosh <m@zyp.io>
2016-09-17 22:23:20 -0700

    refactored bsm out of main

--- main.go
+++ main.go
@@ -8,78 +8,17 @@ import "C"
 
 import (
 	"bufio"
-	"encoding/binary"
 	"flag"
 	"fmt"
 	"log"
 	"os"
-	"strconv"
-	"strings"
-	"time"
+
+	"github.com/mikemackintosh/go-auditx/lib/bsm"
 
 	"bytes"
 )
 
 var debug bool = false
-
-type Token struct {
-	Header32
-	Header64
-	Subject32
-	Text
-}
-
-// SizeHeader32 is the byte size of the header
-var SizeHeader32 int = 17
-
-// Header32 contains standard audit header tokens
-type Header32 struct {
-	RecordLength  uint32
-	Version       byte
-	EventType     uint16
-	EventModifier uint16
-	UnixTimestamp uint32
-	Milliseconds  uint32
-}
-
-// Time generates a time.Time from the unix timestamp
-func (h Header32) Time() time.Time {
-	seconds := int64(h.UnixTimestamp)
-	milliseconds := int64(h.Milliseconds)
-	return time.Unix(seconds, milliseconds)
-}
-
-type Header64 struct {
-	Type          byte
-	RecordLength  uint32
-	Version       uint16
-	EventType     uint16
-	EventModifier uint16
-	UnixTimestamp uint64
-	Milliseconds  uint64
-	Timestamp     time.Time
-}
-
-var SizeSubject32 int = 36
-
-type Subject32 struct {
-	AuditUserID uint32
-	UserID      uint32
-	GroupID     uint32
-	RealUID     uint32
-	RealGID     uint32
-	ProcessID   uint32
-	SessionID   uint32
-	Terminal    struct {
-		PortID    uint32
-		MachineID [4]byte
-	}
-}
-
-type Text struct {
-	Size uint16
-	Data string
-}
 
 type Return32 struct {
 	Status byte
@@ -100,7 +39,7 @@ func main() {
 
 	fmt.Println("==")
 
-	fd, err := os.Open(AUDIT_PIPE)
+	fd, err := os.Open(bsm.AUDIT_PIPE)
 	if err != nil {
 		log.Printf("Err: %s", err)
 	}
@@ -124,22 +63,22 @@ func main() {
 
 		var reclen uint32
 		var eventType byte
-		token := &Token{}
+		token := &bsm.Token{}
 
 		for reclen = uint32(buf.Len()); reclen > 0; reclen, eventType, _ = parseRecord(buf) {
 			switch eventType {
-			case AUT_HEADER32:
-				if err := parseHeader32(buf, token); err != nil {
+			case bsm.AUT_HEADER32:
+				if err := bsm.ParseHeader32(buf, token); err != nil {
 					fmt.Printf("Header parsing error: %+s", err)
 				}
 
-			case AUT_SUBJECT32:
-				if err := parseSubject32(buf, token); err != nil {
+			case bsm.AUT_SUBJECT32:
+				if err := bsm.ParseSubject32(buf, token); err != nil {
 					fmt.Printf("Subject parsing error: %+s", err)
 				}
 
-			case AUT_TEXT:
-				if err := parseText(buf, token); err != nil {
+			case bsm.AUT_TEXT:
+				if err := bsm.ParseText(buf, token); err != nil {
 					fmt.Printf("Text parsing error: %+s", err)
 				}
 			}
@@ -164,14 +103,14 @@ func parseRecord(buf *bytes.Buffer) (uint32, byte, error) {
 
 	// Let's wet the lips
 	if debug {
-		fmt.Printf("> Found new token type of: %s (%d)\n\n", TokenTypeDictionary[eventType], eventType)
+		fmt.Printf("> Found new token type of: %s (%d)\n", bsm.TokenTypeDictionary[eventType], eventType)
 	}
 
 	switch eventType {
-	case AUT_HEADER32,
-		AUT_HEADER32_EX,
-		AUT_HEADER64,
-		AUT_HEADER64_EX:
+	case bsm.AUT_HEADER32,
+		bsm.AUT_HEADER32_EX,
+		bsm.AUT_HEADER64,
+		bsm.AUT_HEADER64_EX:
 
 		// Read the next few bytes of the header token
 		// Get the record length
@@ -179,7 +118,7 @@ func parseRecord(buf *bytes.Buffer) (uint32, byte, error) {
 
 		break
 
-	case AUT_OTHER_FILE32:
+	case bsm.AUT_OTHER_FILE32:
 		if sec := readUint32(buf); sec < sizeofUint32 {
 			return 0, eventType, nil
 		}
@@ -193,41 +132,6 @@ func parseRecord(buf *bytes.Buffer) (uint32, byte, error) {
 	}
 
 	return reclen, eventType, nil
-}
-
-// parseHeader32
-func parseHeader32(buf *bytes.Buffer, tok *Token) error {
-	h := Header32{}
-	data := readNextBytes(buf, SizeHeader32)
-	buffer := bytes.NewBuffer(data)
-	err := binary.Read(buffer, binary.BigEndian, &h)
-	if err != nil {
-		return err
-	}
-	// Set the header in the token
-	tok.Header32 = h
-	return nil
-}
-
-// parseSubject32 parses the user/actor subject
-func parseSubject32(buf *bytes.Buffer, tok *Token) error {
-	subject := Subject32{}
-	data := readNextBytes(buf, SizeSubject32)
-	buffer := bytes.NewBuffer(data)
-	err := binary.Read(buffer, binary.BigEndian, &subject)
-	if err != nil {
-		return err
-	}
-	// Set the subject in the token
-	tok.Subject32 = subject
-	return nil
-}
-
-// parseText parses the text object, can be variable length
-func parseText(buf *bytes.Buffer, tok *Token) error {
-	tok.Text.Size = readUint16(buf)
-	tok.Text.Data = string(buf.Next(int(tok.Text.Size)))
-	return nil
 }
 
 func ByteToInt64(array []byte) uint64 {
@@ -258,52 +162,4 @@ func ByteToInt16(array []byte) uint16 {
 		out |= uint16(b) << shift
 	}
 	return out
-}
-
-type EventsDictionary map[uint16]EventDefinition
-type EventDefinition struct {
-	ID       uint16
-	Constant string
-	Name     string
-	Flag     []string
-}
-
-func ParseEventsFile(eventsFile string) (EventsDictionary, error) {
-	var ed = EventsDictionary{}
-
-	if _, err := os.Stat(eventsFile); err == nil {
-		file, err := os.Open(eventsFile)
-		if err != nil {
-			return ed, fmt.Errorf("No events file found.")
-		}
-		defer file.Close()
-
-		scanner := bufio.NewScanner(file)
-		for scanner.Scan() {
-			// trim the line from all leading whitespace first
-			line := strings.TrimLeft(scanner.Text(), " \t")
-			// line is not empty, and not starting with '#'
-			if len(line) > 0 && !strings.HasPrefix(line, "#") {
-				event := strings.SplitN(line, ":", 4)
-				if len(event) == 4 {
-					eventID, _ := strconv.ParseInt(event[0], 10, 16)
-					eid := uint16(eventID)
-					definition := EventDefinition{
-						ID:       eid,
-						Constant: event[1],
-						Name:     event[2],
-						Flag:     strings.Split(event[3], ","),
-					}
-					ed[eid] = definition
-				}
-			}
-		}
-
-		// If there was an error running scan, then return
-		if err := scanner.Err(); err != nil {
-			return ed, fmt.Errorf("Unable to read events file.")
-		}
-	}
-
-	return ed, nil
 }
--- main_test.go
+++ main_test.go
@@ -1,70 +1,7 @@
 package main
 
-import (
-	"bytes"
-	"testing"
-)
+import "testing"
 
-// parseHeader32
-var header32Bytes = []byte{0, 0, 0, 127, 11, 24, 63, 0, 0, 87, 221, 212, 203, 0, 0, 1, 253}
+func TestMain(t *testing.T) {
 
-var header32Test = []struct {
-	byteBuffer *bytes.Buffer
-	expected   Header32
-}{
-	{bytes.NewBuffer(header32Bytes), Header32{
-		RecordLength:  127,
-		Version:       11,
-		EventType:     6207,
-		EventModifier: 0,
-		UnixTimestamp: 1474155723,
-		Milliseconds:  509,
-	}},
-}
-
-func TestParseHeader32(t *testing.T) {
-	for i, tt := range header32Test {
-		token := &Token{}
-		if err := parseHeader32(tt.byteBuffer, token); err != nil {
-			t.Fatalf("parseHeader32(%d): failed parsing header", i)
-		}
-		if token.Header32 != tt.expected {
-			t.Errorf("parseHeader32(%d): expected %+v, actual %+v", i, tt.expected, token.Header32)
-		}
-	}
-}
-
-var subject32Bytes = []byte{0, 0, 1, 245, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 57, 104, 0, 1, 134, 166, 0, 1, 191, 40, 0, 0, 0, 0}
-var subject32Test = []struct {
-	byteBuffer *bytes.Buffer
-	expected   Subject32
-}{
-	{bytes.NewBuffer(subject32Bytes), Subject32{
-		AuditUserID: 501,
-		UserID:      0,
-		GroupID:     0,
-		RealUID:     0,
-		RealGID:     0,
-		ProcessID:   14696,
-		SessionID:   100006,
-		Terminal: struct {
-			PortID    uint32
-			MachineID [4]byte
-		}{
-			PortID:    114472,
-			MachineID: [4]byte{0, 0, 0, 0},
-		},
-	}},
-}
-
-func TestParseSubject32(t *testing.T) {
-	for i, tt := range subject32Test {
-		token := &Token{}
-		if err := parseSubject32(tt.byteBuffer, token); err != nil {
-			t.Fatalf("parseSubject32(%d): failed parsing header", i)
-		}
-		if token.Subject32 != tt.expected {
-			t.Errorf("parseSubject32(%d): expected %+v, actual %+v", i, tt.expected, token.Subject32)
-		}
-	}
 }
