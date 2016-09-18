4392477b933e203c9b39da444144873670b3a492
Mike Mackintosh <m@zyp.io>
2016-09-17 23:11:44 -0700

    add marshalling to headers

--- lib/bsm/header.go
+++ lib/bsm/header.go
@@ -11,21 +11,22 @@ var SizeHeader32 int = 17
 
 // Header contains standard audit header tokens
 type Header struct {
-	Header32
-	EventName string
+	Header32  `json:"header" xml:"header"`
+	EventName string    `json:"name" xml:"name"`
+	Timestamp time.Time `json:"timestamp" xml:"timestamp"`
 }
 
 type Header32 struct {
-	RecordLength  uint32
-	Version       byte
-	EventType     uint16
-	EventModifier uint16
-	UnixTimestamp uint32
-	Milliseconds  uint32
+	RecordLength  uint32 `json:"len" xml:"len"`
+	Version       byte   `json:"version" xml:"version"`
+	EventType     uint16 `json:"type" xml:"type"`
+	EventModifier uint16 `json:"modifier" xml:"modifier"`
+	UnixTimestamp uint32 `json:"unixtime" xml:"unixtime"`
+	Milliseconds  uint32 `json:"milliseconds" xml:"milliseconds"`
 }
 
-// Time generates a time.Time from the unix timestamp
-func (h Header32) Time() time.Time {
+// Timestamp generates a time.Time from the unix timestamp
+func (h Header32) Timestamp() time.Time {
 	seconds := int64(h.UnixTimestamp)
 	milliseconds := int64(h.Milliseconds)
 	return time.Unix(seconds, milliseconds)
@@ -43,5 +44,8 @@ func ParseHeader32(buf *bytes.Buffer, tok *Token) error {
 	// Set the header in the token
 	tok.Header = Header{Header32: h}
 	tok.Header.EventName = EventTypes[h.EventType].Name
+	// Set the timestamp
+	timestamp := h.Timestamp()
+	tok.Header.Timestamp = timestamp
 	return nil
 }
