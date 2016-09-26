de1db476cfb382011b84a826566301107a85ac72
Mike Mackintosh <m@zyp.io>
2016-09-25 21:48:11 -0700

    add socket support

---
+++ lib/bsm/socket.go
@@ -0,0 +1,43 @@
+package bsm
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+
+	"github.com/mikemackintosh/go-auditx/lib/config"
+)
+
+var SizeofSocket int = 18
+
+type Socket struct {
+	Domain        uint16 `json:"sock_domain" xml:"socket>sock_domain"`
+	SocketType    uint16 `json:"sock_type" xml:"socket>sock_type"`
+	AddressType   uint16 `json:"addr_type" xml:"socket>addr_type"`
+	LocalPort     uint16 `json:"local_port" xml:"socket>local_port"`
+	LocalAddress  uint16 `json:"local_address" xml:"socket>local_address"`
+	RemotePort    uint16 `json:"report_port" xml:"socket>report_port"`
+	RemoteAddress uint16 `json:"remote_address" xml:"socket>remote_address"`
+}
+
+// ParseSocket parses the socket object, can be variable length
+func ParseSocket(buf *bytes.Buffer, tok *Token) error {
+	s := Socket{}
+
+	data := readNextBytes(buf, SizeofSocket)
+	buffer := bytes.NewBuffer(data)
+	err := binary.Read(buffer, binary.BigEndian, &s)
+	if err != nil {
+		return err
+	}
+
+	// Set the header in the token
+	tok.Socket = s
+
+	if config.Debug {
+		fmt.Printf("> Buffer: %+v", data)
+		fmt.Printf("> Struct: %+v", s)
+	}
+
+	return nil
+}
--- lib/bsm/token.go
+++ lib/bsm/token.go
@@ -1,10 +1,11 @@
 package bsm
 
 type Token struct {
+	Arg     []Arg `json:"arg" xml:"arg"`
 	Header  `json:"header" xml:"header"`
-	Subject `json:"subject" xml:"subject"`
-	Arg     `json:"arg" xml:"arg"`
-	Text    []Text `json:"text" xml:"text"`
 	Return  `json:"return" xml:"return"`
+	Socket  `json:"socket" xml:"socket"`
+	Subject `json:"subject" xml:"subject"`
+	Text    []Text `json:"text" xml:"text"`
 	Trailer `json:"trailer" xml:"trailer"`
 }
