acd4422c09513c73234f64849a532cc5ad8b0d4f
Mike Mackintosh <m@zyp.io>
2016-10-01 16:23:43 -0700

    make testing better

--- Makefile
+++ Makefile
@@ -1,7 +1,12 @@
 all: test build
 
 test:
-	go test -v ./...
+	go test -cover -v ./...
+
+coverage:
+	go test -covermode=count -coverprofile=coverage.out
+	go test -covermode=count -coverprofile=coverage.out ./lib/bsm/...
+	go tool cover -html=coverage.out
 
 build:
 	go build -o bin/go-auditx *.go
--- lib/bsm/header_test.go
+++ lib/bsm/header_test.go
@@ -8,8 +8,8 @@ import (
 
 // parseHeader32
 var header32Bytes = []byte{0, 0, 0, 127, 11, 24, 63, 0, 0, 87, 221, 212, 203, 0, 0, 1, 253}
-
-//var timestamp = "2016-09-17 16:42:03.000000509 -0700 PDT"
+var timestampFormat = "2006-01-02T15:04:05.999999-07:00"
+var timestamp = "2016-09-17T16:42:03.000000509-07:00"
 
 var header32Test = []struct {
 	byteBuffer *bytes.Buffer
@@ -27,15 +27,14 @@ var header32Test = []struct {
 }
 
 func TestParseHeader32(t *testing.T) {
+	parsedTimestamp, _ := time.Parse(timestampFormat, timestamp)
+
 	for i, tt := range header32Test {
 		token := &Token{}
 		if err := ParseHeader32(tt.byteBuffer, token); err != nil {
 			t.Fatalf("parseHeader32(%d): failed parsing header", i)
 		}
 
-		timestampFmt := "2006-01-02T15:04:05.999999-07:00"
-		str := "2016-09-17T16:42:03.000000509-07:00"
-		parsedTimestamp, _ := time.Parse(timestampFmt, str)
 		tt.expected.Timestamp = parsedTimestamp
 
 		if token.Header != tt.expected {
