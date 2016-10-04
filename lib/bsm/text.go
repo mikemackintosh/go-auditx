dda0644b6d886b3bdfae66ceaa8a4964fa4413b9
Mike Mackintosh <m@zyp.io>
2016-10-03 22:11:21 -0700

    add debugging output to text

--- lib/bsm/text.go
+++ lib/bsm/text.go
@@ -2,7 +2,11 @@ package bsm
 
 import (
 	"bytes"
+	"encoding/binary"
+	"fmt"
 	"strings"
+
+	"github.com/mikemackintosh/go-auditx/lib/config"
 )
 
 type Text struct {
@@ -14,7 +18,15 @@ type Text struct {
 func ParseText(buf *bytes.Buffer, tok *Token) error {
 	text := Text{}
 	text.Length = ReadUint16(buf)
-	text.Data = strings.TrimSuffix(string(buf.Next(int(text.Length))), "\u0000")
+	textBytes := buf.Next(int(text.Length))
+
+	text.Data = strings.TrimSuffix(string(textBytes), "\\u0000")
+
+	if config.Debug {
+		b := make([]byte, 2)
+		binary.LittleEndian.PutUint16(b, text.Length)
+		fmt.Printf("> Bytes: %v %v", b, textBytes)
+	}
 
 	tok.Text = append(tok.Text, text)
 	return nil
