975c5c8afef371569ed503d6d123efa310db2fbc
Mike Mackintosh <m@zyp.io>
2016-10-03 22:13:24 -0700

    add notest to token

--- lib/bsm/token.go
+++ lib/bsm/token.go
@@ -1,8 +1,10 @@
 package bsm
 
+// Token is the base audit record
 type Token struct {
 	Arg     []Arg `json:"arg,omitempty" xml:"arg"`
 	Header  `json:"header,omitempty" xml:"header"`
+	Path    []Path `json:"path,omitempty" xml:"path"`
 	Return  `json:"return,omitempty" xml:"return"`
 	Socket  `json:"socket,omitempty" xml:"socket"`
 	Subject `json:"subject,omitempty" xml:"subject"`
